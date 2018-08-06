c490d9953fb6e75ac39da8bef6f2834817f46e27
newcoderlife <newcoderlife@outlook.com>
2018-08-06 11:23:21 +0800

    feat(src/app/news): add 'Article' model
    Add 'Article' model for showing news.

--- src/app/news/models.py
+++ src/app/news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    excerpt = models.TextField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
