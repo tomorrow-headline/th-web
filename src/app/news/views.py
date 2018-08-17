bc74ee0c72d2a049e391897bfba04b92ac5cb399
newcoderlife <newcoderlife@outlook.com>
2018-08-17 14:21:33 +0800

    feat(src/app/news): add 'CommentViewSet' for 'Comment' model

--- src/app/news/views.py
+++ src/app/news/views.py
@@ -6,3 +6,8 @@ from news import models, serializers
 class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Article.objects.all()
     serializer_class = serializers.ArticleSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
