a7d186d4b7027a16e931461eb3f85b44ab2276c1
newcoderlife <newcoderlife@outlook.com>
2018-08-30 16:31:27 +0800

    feat(src/app/news): parse user and article info to CommentSerializer

--- src/app/news/views.py
+++ src/app/news/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 
 from news import models, serializers
 
@@ -11,6 +13,21 @@ class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = models.Comment.objects.all()
     serializer_class = serializers.CommentSerializer
+
+    def create(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        request.data['author'] = request.user.username
+        request.data['ref'] = get_object_or_404(
+            models.Article,
+            pk=request.data['article_pk']
+        )
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
