5051cbdccb5fd0f3e928cfa4e698997beda8e298
newcoderlife <newcoderlife@outlook.com>
2018-09-13 14:08:17 +0800

    refactor(src/app): add static dir to settings.py

--- src/app/tomorrow_headline/settings.py
+++ src/app/tomorrow_headline/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tomorrow_headline.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.abspath(os.path.join(BASE_DIR, '../appfront/'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../appfront/static/'))
