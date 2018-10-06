8b07a6a034829b77a640c150675953b7562837d1
newcoderlife <newcoderlife@outlook.com>
2018-10-06 16:23:05 +0800

    update static file settings

--- src/tomorrow_headline/settings.py
+++ src/tomorrow_headline/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tomorrow_headline.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.abspath(os.path.join(BASE_DIR, '../appfront/'))],
+        'DIRS': [os.path.abspath(os.path.join(BASE_DIR, '../../th-web-frontend/dist/'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -120,4 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../appfront/static/'))
+STATIC_ROOT = os.path.abspath(
+    os.path.join(BASE_DIR, '../../th-web-frontend/dist/')
+)
+STATICFILES_DIRS = (
+    os.path.abspath(
+        os.path.join(BASE_DIR, '../../th-web-frontend/dist/static/')
+    ),
+)
